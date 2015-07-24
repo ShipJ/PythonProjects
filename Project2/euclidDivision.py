0bafe59b7c77495d32bc4676185727639f8eb8c6
Jack <jack.shipway.12@ucl.ac.uk>
2015-07-24 12:14:14 +0100

    Added Euclidean Division Algorithm

---
+++ Project2/euclidDivision.py
@@ -0,0 +1,42 @@
+__author__ = 'Jack'
+
+import time
+
+
+
+#Euclids algorithm for the greatest common divisor
+    # m = int(input("Enter your first integer: "))
+    # n = int(input("Enter your second integer: "))
+
+def gcd(m,n):
+    x,y,r = m,n,m%n
+    if r == 0:
+        print("%d is a multiple of %d, %d is your answer." % (m,n,n))
+    while r != 0:
+        if r == 1:
+            print("The answer is 1. Ahh, that means %d and %d are coprime" % (x,y))
+            break
+        else:
+            m = n
+            n = r
+            r = m % n
+        if r == 0:
+            print("The greatest common divisor of %d and %d is %d." % (x,y,n))
+
+start_time = time.time()
+gcd(2342343241, 5144)
+gcd(235423,2434)
+gcd(235423,2434)
+gcd(235423,2434)
+gcd(235423,2434)
+gcd(235423,2434)
+gcd(235423,2434)
+gcd(235423,2434)
+gcd(235423,2434)
+gcd(235423,2434)
+gcd(235423,2434)
+gcd(235423,2434)
+gcd(235423,2434)
+print("-- The process took %s seconds --" % (time.time() - start_time))
+
+
